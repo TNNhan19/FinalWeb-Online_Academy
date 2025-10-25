8c7bfb1e0fe599f6a790dfd61fa04bb7e3a703ad
Trieudz2k5 <trieu03052000@gmail.com>
2025-10-26 04:45:40 +0700

    Xây dựng danh sách watchlist yêu thích, các chức năng xem và loại bỏ khóa học ở profile, 26/10/2025

---
+++ models/students.model.js
@@ -0,0 +1,21 @@
+// models/students.model.js
+import db from "../configs/db.js";
+import bcrypt from "bcrypt";
+
+const TABLE = "students";
+
+export async function getById(id) {
+  return await db(TABLE).where({ id }).first();
+}
+
+export async function updateProfile(id, data) {
+  return await db(TABLE).where({ id }).update(data);
+}
+
+export async function updatePassword(id, oldPass, newPass) {
+  const user = await getById(id);
+  const match = await bcrypt.compare(oldPass, user.password);
+  if (!match) throw new Error("Sai mật khẩu cũ");
+  const hashed = await bcrypt.hash(newPass, 10);
+  return await db(TABLE).where({ id }).update({ password: hashed });
+}
