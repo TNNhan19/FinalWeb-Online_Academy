d3edcc3468910e63d71acb56c47fdef0430325da
Ducduyzs <ducduyzs1102@gmail.com>
2025-10-26 11:11:45 +0700

    Update db.js
    Update db.js for connect

--- configs/db.js
+++ configs/db.js
@@ -1,14 +1,11 @@
 import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "db.ndbcbzxoqstwxhjgwpyj.supabase.co",      
+  port: 5432,                  
+  user: "postgres",            
+  password: "ndbcbzxoqstwxhjgwpyj",  
+  database: "postgres",  
   ssl: { rejectUnauthorized: false },
 });
