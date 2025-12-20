2d11887bf04dec36ab574548e47d76e7b8a29b92
TNNhan19 <nhantn1919@gmail.com>
2025-12-20 23:02:02 +0700

    undo

--- app.js
+++ app.js
@@ -190,11 +190,7 @@ app.use(
     secret: process.env.SESSION_SECRET || "supersecretkey",
     resave: false,
     saveUninitialized: false,
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "none",
-      secure: true,
-    },
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
   })
 );
 
